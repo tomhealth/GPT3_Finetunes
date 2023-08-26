85c64b13f19d12dec353f0a292dd6f4b16f83ec6
tomhealth <tomhealth2022@gmail.com>
2023-08-26 06:33:00 +0000

    gpt4 and mental health

--- Compassionate_Chatbot/synthesize_convos.py
+++ Compassionate_Chatbot/synthesize_convos.py
@@ -9,47 +9,44 @@ def open_file(filepath):
 
 
 def save_convo(text, topic):
-    with open('finetuning/%s_%s.txt' % (topic, time()), 'w', encoding='utf-8') as outfile:
+    with open('finetuning_mt3/%s_%s.txt' % (topic, time()), 'w', encoding='utf-8') as outfile:
         outfile.write(text)
 
 
-openai.api_key = open_file('openaiapikey.txt')
+openai.api_key = "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"
 
-
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=1000, freq_pen=0.0, pres_pen=0.0, stop=['<<END>>']):
+def gpt3_completion(prompt, engine='gpt-4', temp=0.7, top_p=0.5, tokens=1000, freq_pen=0.0, pres_pen=0.0, stop=['<<END>>']):
     max_retry = 5
     retry = 0
     while True:
         try:
-            response = openai.Completion.create(
-                engine=engine,
-                prompt=prompt,
-                temperature=temp,
-                max_tokens=tokens,
-                top_p=top_p,
-                frequency_penalty=freq_pen,
-                presence_penalty=pres_pen,
-                stop=stop)
-            text = response['choices'][0]['text'].strip()
-            filename = '%s_gpt3.txt' % time()
-            with open('gpt3_logs/%s' % filename, 'w') as outfile:
-                outfile.write('PROMPT:\n\n' + prompt + '\n\n==========\n\nRESPONSE:\n\n' + text)
+            message=[{"role": "user", "content": prompt}]
+            response = openai.ChatCompletion.create(
+                model="gpt-4",
+                messages = message,
+                temperature=0.9,
+                max_tokens=3000,
+                frequency_penalty=0.0
+                )
+            
+            print(response)
+            text = response['choices'][0]['message']['content'].strip()
             return text
         except Exception as oops:
             retry += 1
             if retry >= max_retry:
-                return "GPT3 error: %s" % oops
+                return "GPT4 error: %s" % oops
             print('Error communicating with OpenAI:', oops)
             sleep(0.25)
 
 
 if __name__ == '__main__':
-    topics = open_file('topics.txt').splitlines()
+    topics = open_file('topics2.txt').splitlines()
     for topic in topics:
         print(topic)
-        prompt = open_file('syn_prompt2.txt').replace('<<TOPIC>>', topic)
+        prompt = open_file('syn_prompt1.txt').replace('<<TOPIC>>', topic)
         response = gpt3_completion(prompt)
-        outtext = 'User: Hey EVE%s' % response
+        outtext = '%s' % response
         print(outtext)
         tpc = topic.replace(' ', '')[0:15]
         save_convo(outtext, tpc)
